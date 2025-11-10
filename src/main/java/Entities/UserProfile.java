49f8cf0f0526f40c002fbd4dec71ee75b3bc94ae
Eren Filiz <erenfiliz1907@gmail.com>
2025-11-09 21:59:24 -0500

    Created the abstract parent class UserProfile and its children classes JobseekerProfile and EmployerProfile

---
+++ src/main/java/Entities/UserProfile.java
@@ -0,0 +1,51 @@
+package Entities;
+
+abstract class UserProfile {
+    Integer userId;
+    String email;
+    String phoneNumber;
+    String profileUsername;
+
+    protected UserProfile(Integer userId, String email, String phoneNumber, String profileUsername) {
+        this.userId = userId;
+        this.email = email;
+        this.phoneNumber = phoneNumber;
+        this.profileUsername = profileUsername;
+    }
+
+    public void setUserId(Integer userId) {this.userId = userId; }
+
+    public void setEmail(String email) {this.email = email; }
+
+    public void setPhoneNumber(String phoneNumber) {this.phoneNumber = phoneNumber; }
+
+    public void setProfileUsername(String profileUsername) {this.profileUsername = profileUsername; }
+
+    public Integer getUserId() {return this.userId; }
+
+    public String getEmail() { return this.email; }
+
+    public String getPhoneNumber() { return this.phoneNumber; }
+
+    public String getProfileUsername() { return this.profileUsername; }
+
+    public abstract String getFullProfileAsString();
+
+    public abstract String getSummaryProfileAsString();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
