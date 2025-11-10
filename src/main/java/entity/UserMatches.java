228de3ace20deb17f68732b73a19e352179430fe
Kyrylo <kirilldegt2005@gmail.com>
2025-11-10 00:36:23 -0500

    Create userMatches entity (issue #17)

---
+++ src/main/java/entity/UserMatches.java
@@ -0,0 +1,43 @@
+package entity;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class UserMatches {
+    private String username;
+    private int userId;
+    private ArrayList<Integer> matchIds;
+    private ArrayList<Integer> approvedIds;
+    private ArrayList<Integer> rejectedIds;
+
+    public UserMatches() {
+        this.matchIds = new ArrayList<>();
+        this.approvedIds = new ArrayList<>();
+        this.rejectedIds = new ArrayList<>();
+    }
+
+    public List<Integer> getMatches() {
+        return matchIds;
+    }
+
+    public List<Integer> getApproved() {
+        return approvedIds;
+    }
+
+    public List<Integer> getRejected() {
+        return rejectedIds;
+    }
+
+    // Add methods
+    public void addMatch(int matchId) {
+        matchIds.add(matchId);
+    }
+
+    public void addApproved(int approvalId) {
+        approvedIds.add(approvalId);
+    }
+
+    public void addRejected(int declineId) {
+        rejectedIds.add(declineId);
+    }
+}
