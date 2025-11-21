5f584c709a7d922bf33de03ee1614f650a27ade3
wyattwong-git <wongwyattl@gmail.com>
2025-11-21 17:10:22 -0500

    Update MatchPage view and state

--- src/main/java/interface_adapter/match_page/MatchPageState.java
+++ src/main/java/interface_adapter/match_page/MatchPageState.java
@@ -1,5 +1,32 @@
 package interface_adapter.match_page;
 
-public class MatchPageState {
+import java.util.ArrayList;
 
+public class MatchPageState {
+    private Integer currentId;
+    private ArrayList<UserMatches> matchUser;
+    private ArrayList<String> userMatchesName;
+    private String resume;
+
+    public Integer getCurrentId() { return currentId; }
+
+    public ArrayList<UserMatches> getUserMatches() { return matchUser; }
+
+    public ArrayList<String> getUserMatchesName() { return userMatchesName; }
+
+    public String getResume() { return resume; }
+
+    public void setCurrentId(Integer currentId) {
+        this.currentId = currentId;
+    }
+
+    public void setUserMatches(ArrayList<UserMatches> matchUser) {
+        this.matchUser = matchUser;
+    }
+
+    public void setUserMatchesName(ArrayList<String> userMatchesName) {
+        this.userMatchesName = userMatchesName;
+    }
+
+    public void setResume (String resume) { this.resume = resume; }
 }
