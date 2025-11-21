219ec14f38bbd3e2643b263e52a025e76d0119b2
chenfengyuan <fengyuan2023@hotmail.com>
2025-11-21 15:20:02 -0500

    update

--- src/main/java/interface_adapter/match_page/MatchPageController.java
+++ src/main/java/interface_adapter/match_page/MatchPageController.java
@@ -1,4 +1,25 @@
 package interface_adapter.match_page;
 
+import use_case.approve_reject_profile.ApproveRejectProfileInputData;
+import use_case.recommend_profile.RecommendProfileInputData;
+import use_case.update_matches.*;
+
 public class MatchPageController {
+
+    private final UpdateMatchesInputBoundary updateMatchesUseCaseInteractor;
+
+    public MatchPageController(UpdateMatchesInputBoundary updateMatchesUseCaseInteractor) {
+        this.updateMatchesUseCaseInteractor = updateMatchesUseCaseInteractor;
+    }
+
+    /**
+     * Executes the Home Screen Use Case.
+     * @param
+     */
+    public void execute(Integer currentId, Integer otherId, boolean isApprove) {
+        final UpdateMatchesInputData updateMatchesInputData =
+                new ApproveRejectProfileInputData(currentId, otherId, isApprove);
+        updateMatchesUseCaseInteractor.execute(approveRejectProfileInputData);
+
+    }
 }
