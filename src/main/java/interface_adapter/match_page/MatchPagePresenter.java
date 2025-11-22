bf7867f4777eb52315e1acf5ba28bcc641510bf2
wyattwong-git <wongwyattl@gmail.com>
2025-11-21 19:33:46 -0500

    Implement MatchPagePresenter

--- src/main/java/interface_adapter/match_page/MatchPagePresenter.java
+++ src/main/java/interface_adapter/match_page/MatchPagePresenter.java
@@ -1,4 +1,28 @@
 package interface_adapter.match_page;
 
-public class MatchPagePresenter {
+import use_case.update_matches.UpdateMatchesOutputBoundary;
+import use_case.update_matches.UpdateMatchesOutputData;
+
+public class MatchPagePresenter implements UpdateMatchesOutputBoundary {
+
+    private final MatchPageViewModel matchPageViewModel;
+    //    private final ViewManagerModel viewManagerModel;
+
+    public MatchPagePresenter(MatchPageViewModel matchPageViewModel) {
+        this.matchPageViewModel = matchPageViewModel;
+        //        this.viewManagerModel = viewManagerModel; ViewManagerModel viewManagerModel,
+    }
+
+    @Override
+    public void prepareSuccessView(UpdateMatchesOutputData response) {
+        final MatchPageState matchPageState = matchPageViewModel.getState();
+        matchPageState.setCurrentId(response.getUserProfile().getUserId());
+        matchPageState.setEmail(response.getUserProfile().getEmail());
+        matchPageState.setName(response.getUserProfile().getProfileUsername());
+        matchPageState.setPhone(response.getUserProfile().getPhoneNumber());
+        matchPageState.setResume(response.getUserProfile().getSummaryProfileAsString());
+        // matchPageState.setUserMatches(response.);
+        // matchPageState.setUserMatchesName(response.);
+        this.matchPageViewModel.firePropertyChange();
+    }
 }
