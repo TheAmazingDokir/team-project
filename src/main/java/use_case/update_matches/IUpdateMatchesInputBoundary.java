9f6d46312e726fe8aba614ac80ced581c7aeb7d7
Maher Rahman <maherahman3900@gmail.com>
2025-11-24 10:20:40 -0500

    #50, Updated the names of the files for the Update Matches use case

--- src/main/java/use_case/update_matches/IUpdateMatchesInputBoundary.java
+++ src/main/java/use_case/update_matches/IUpdateMatchesInputBoundary.java
@@ -1,9 +1,5 @@
 package use_case.update_matches;
 
-import entity.UserProfile;
-
-import java.util.ArrayList;
-
 /**
  * Input boundary for actions related to updating matches
  **/
@@ -14,12 +10,5 @@ public interface IUpdateMatchesInputBoundary {
      * @param user1Id the first user in the match
      * @param user2Id the second user in the match
      */
-    void createMatch(int user1Id, int user2Id);
-
-    /**
-     * Finds all the people this user has approved, and returns their User Profile
-     * @param userId the user whose approved profiles are desired
-     * @return an ArrayList of the UserProfile objects of the users this user has approved
-     */
-    ArrayList<UserProfile>  userIdtoUserMatchesProfiles(int userId);
+    void execute(int user1Id, int user2Id);
 }
