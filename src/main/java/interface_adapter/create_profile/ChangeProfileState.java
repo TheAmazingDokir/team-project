8871e0f5fe695417dfe3e169c5c001ea9720f7a0
Milan White <milan.white.25@gmail.com>
2025-11-24 14:00:05 -0500

    feat: add view manager functionality to change profile presenter & controller, fix home view typo, update change profile state to have profileUsername (#22)

--- src/main/java/interface_adapter/create_profile/ChangeProfileState.java
+++ src/main/java/interface_adapter/create_profile/ChangeProfileState.java
@@ -6,16 +6,19 @@ public class ChangeProfileState {
     private String summary;
     private String profile;
     private Boolean isEmployer;
+    private String profileUsername;
 
     public void setEmail(String email) {this.email = email;}
     public void setPhoneNumber(String phoneNumber) {this.phoneNumber = phoneNumber;}
     public void setSummary(String summary) {this.summary = summary;}
     public void setProfile(String profile) {this.profile = profile;}
     public void setIsEmployer(Boolean isEmployer) {this.isEmployer = isEmployer;}
+    public void setProfileUsername(String profileUsername) {this.profileUsername = profileUsername;}
 
     public String getEmail() {return email;}
     public String getPhoneNumber() {return phoneNumber;}
     public String getSummary() {return summary;}
     public String getProfile() {return profile;}
     public Boolean getIsEmployer() {return isEmployer;}
+    public String getProfileUsername() {return profileUsername;}
 }
