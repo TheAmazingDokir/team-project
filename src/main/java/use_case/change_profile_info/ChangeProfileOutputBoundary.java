3d1f12232854fffed67fc2b5bf05c83d2a8c3122
Eren Filiz <erenfiliz1907@gmail.com>
2025-11-24 10:52:00 -0500

    Fixed typo

--- src/main/java/use_case/change_profile_info/ChangeProfileOutputBoundary.java
+++ src/main/java/use_case/change_profile_info/ChangeProfileOutputBoundary.java
@@ -3,7 +3,7 @@ package use_case.change_profile_info;
 /**
  * The output boundary for the ChangeProfile Use Case.
  */
-public interface ChangeProfileOutputBoundry {
+public interface ChangeProfileOutputBoundary {
     /**
      * Prepares the success view for the ChangeProfile Use Case.
      * @param outputData the output data
