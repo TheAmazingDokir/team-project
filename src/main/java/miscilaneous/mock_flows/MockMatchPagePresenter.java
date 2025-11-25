4e711ff02a05f7012dc029b07a0c442d28eaa3c2
chenfengyuan <fengyuan2023@hotmail.com>
2025-11-25 00:19:32 -0500

    mock MatchPage

---
+++ src/main/java/miscilaneous/mock_flows/MockMatchPagePresenter.java
@@ -0,0 +1,11 @@
+package miscilaneous.mock_flows;
+
+
+import use_case.update_matches.UpdateMatchesOutputBoundary;
+import use_case.update_matches.UpdateMatchesOutputData;
+
+public class MockMatchPagePresenter implements UpdateMatchesOutputBoundary {
+    public void prepareSuccessView(UpdateMatchesOutputData updateMatchesOutputData) {
+
+    }
+}
