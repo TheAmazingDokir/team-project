9e150e8358cdebc0e4e9d365e89c5289e36ddc42
Milan White <milan.white.25@gmail.com>
2025-11-28 15:02:11 -0500

    route from create profile to home screen on success (#67)

--- src/main/java/interface_adapter/create_profile/ChangeProfilePresenter.java
+++ src/main/java/interface_adapter/create_profile/ChangeProfilePresenter.java
@@ -1,6 +1,10 @@
 package interface_adapter.create_profile;
 
 import interface_adapter.ViewManagerModel;
+import interface_adapter.ViewManagerState;
+import interface_adapter.home_screen.HomeScreenState;
+import interface_adapter.home_screen.HomeScreenViewModel;
+import interface_adapter.login.LoginState;
 import use_case.change_profile_info.ChangeProfileOutputBoundary;
 import use_case.change_profile_info.ChangeProfileOutputData;
 
@@ -10,9 +14,13 @@ public class ChangeProfilePresenter implements ChangeProfileOutputBoundary {
 
     private final ViewManagerModel viewManagerModel;
 
-    public ChangeProfilePresenter(ChangeProfileViewModel changeProfileViewModel, ViewManagerModel viewManagerModel) {
+    private final HomeScreenViewModel homeScreenViewModel;
+
+
+    public ChangeProfilePresenter(ChangeProfileViewModel changeProfileViewModel, ViewManagerModel viewManagerModel, HomeScreenViewModel homeScreenViewModel) {
         this.changeProfileViewModel = changeProfileViewModel;
         this.viewManagerModel = viewManagerModel;
+        this.homeScreenViewModel = homeScreenViewModel;
 
         if (this.viewManagerModel.getState().isHasProfile()) {
             System.out.println("PROFILE ALREADY EXISTS");
@@ -24,7 +32,23 @@ public class ChangeProfilePresenter implements ChangeProfileOutputBoundary {
 
     @Override
     public void prepareSuccessView(ChangeProfileOutputData changeProfileOutputData) {
-        System.out.println("ChangeProfilePresenter prepareSuccessView");
+
+        changeProfileViewModel.setState(new ChangeProfileState());
+
+        // switch to logged in view
+        ViewManagerState state = this.viewManagerModel.getState();
+
+        state.setLoggedIn(true);
+        state.setUserId(changeProfileOutputData.getUserID());
+
+        HomeScreenState homeScreenState = homeScreenViewModel.getState();
+        homeScreenState.setCurrentId(changeProfileOutputData.getUserID());
+        homeScreenViewModel.setState(homeScreenState);
+        homeScreenViewModel.firePropertyChange();
+        state.setCurrentView(homeScreenViewModel.getViewName());
+
+        this.viewManagerModel.setState(state);
+        this.viewManagerModel.firePropertyChange();
     }
 
     @Override
