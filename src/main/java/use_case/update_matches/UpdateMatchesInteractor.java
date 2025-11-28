571fdf076d4991ccc51706a8e6ac94a57a431718
Maher Rahman <maherahman3900@gmail.com>
2025-11-28 14:50:27 -0500

    Issue #73
    Renamed methods in MongoDBUserMatchesDataAccessObject so that they all start with lower case, and changed the method name userIdtoUserMatches to getUserMatchesbyId
    
    Moved the profileDAO in UpdateMatchesInteractor to the constructor so that a new one is not created every time userIdtoUserMatchesProfiles is called.

--- src/main/java/use_case/update_matches/UpdateMatchesInteractor.java
+++ src/main/java/use_case/update_matches/UpdateMatchesInteractor.java
@@ -12,6 +12,7 @@ import java.util.ArrayList;
  */
 public class UpdateMatchesInteractor implements IUpdateMatchesInputBoundary {
     MongoDBUserMatchesDataAccessObject dao = new MongoDBUserMatchesDataAccessObject();
+    MongoDBProfileDataAccessObject profileDao = new MongoDBProfileDataAccessObject();
 
     /**
      * If both users have already approved each other, then add them to each others' matches atrtribute
@@ -19,25 +20,24 @@ public class UpdateMatchesInteractor implements IUpdateMatchesInputBoundary {
      * @param user2Id the second user in the match
      */
     public void createMatch(int user1Id, int user2Id) {
-        UserMatches user1Object = dao.userIdtoUserMatches(user1Id);
-        UserMatches user2Object = dao.userIdtoUserMatches(user2Id);
+        UserMatches user1Object = dao.getUserMatchesbyId(user1Id);
+        UserMatches user2Object = dao.getUserMatchesbyId(user2Id);
 
         if (user1Object.getApproved().contains(user2Id) &&  user2Object.getApproved().contains(user1Id)) {
             user1Object.addMatch(user2Id);
             user2Object.addMatch(user1Id);
 
             // update the User Matches in MongoDB
-            dao.ChangeUserMatchesData(user1Object);
-            dao.ChangeUserMatchesData(user2Object);
+            dao.changeUserMatchesData(user1Object);
+            dao.changeUserMatchesData(user2Object);
         }
     }
 
     public ArrayList<UserProfile> userIdtoUserMatchesProfiles(int userId) {
         // get the provided user's UserMatches object
-        UserMatches userMatches = dao.userIdtoUserMatches(userId);
+        UserMatches userMatches = dao.getUserMatchesbyId(userId);
 
         // get the UserProfile objects of everyone this user has approved
-        MongoDBProfileDataAccessObject profileDao = new MongoDBProfileDataAccessObject();
         ArrayList<UserProfile> approvedProfiles = new ArrayList<>();
 
         for (int approvedId : userMatches.getApproved()) {
