657e33cc607eeaf72b69f8a701e196698a3c885f
Milan White <milan.white.25@gmail.com>
2025-11-28 13:44:24 -0500

    removed duplicate getUserID (#68)

--- src/main/java/use_case/login/LoginOutputData.java
+++ src/main/java/use_case/login/LoginOutputData.java
@@ -5,14 +5,30 @@ package use_case.login;
  */
 public class LoginOutputData {
 
+    private final int userId;
     private final String username;
+    private final boolean hasProfile;
 
-    public LoginOutputData(String username) {
+    public LoginOutputData(int userId, String username, boolean hasProfile) {
+        this.userId = userId;
         this.username = username;
+        this.hasProfile = hasProfile;
+    }
+
+    public int getUserId() {
+        return userId;
+    }
+
+    public int getUserId() {
+        return userId;
     }
 
     public String getUsername() {
         return username;
+    }
+
+    public boolean hasProfile() {
+        return hasProfile;
     }
 
 }
